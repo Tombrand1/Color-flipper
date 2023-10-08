200e329f74d304aa20d18c01a13817bf10268bdf
Tombrand1 <tommyrichie00@gmail.com>
2023-10-08 13:15:27 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var colors = ['RED', 'GREY', 'BLUE', 'GREEN', 'YELLOW', 'BROWN', 'TEAL', 'AZURE', 'CRIMSON', 'SILVER', 'LAVENDER', 'IVORY', 'PURPLE', 'PINK', 'ORANGE', 'ORANGERED',]
+var names = ['red', 'blue', 'green', 'yellow', 'brown']
+
+
+function red(x){
+    document.getElementById("biggerscreen").style.backgroundColor = colors[x]
+}
